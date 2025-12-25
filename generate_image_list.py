1cad3381c78eca53f8ca9eba53fcc620a3066315
Gebre <bisrat.gebre@gmail.com  git config --global user.email bisrat.gebre@gmail.com>
2025-12-25 13:20:17 -0800

    Add image carousel slideshow for all 238 photos

---
+++ generate_image_list.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+# Get all image files from the images directory
+images_dir = 'images'
+image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.JPG', '.JPEG', '.PNG', '.GIF')
+
+image_files = []
+for filename in os.listdir(images_dir):
+    if filename.endswith(image_extensions):
+        image_files.append(filename)
+
+# Sort the files
+image_files.sort()
+
+# Write to JSON file
+with open(os.path.join(images_dir, 'images.json'), 'w') as f:
+    json.dump(image_files, f, indent=2)
+
+print(f"Generated images.json with {len(image_files)} images")
+print("First 10 images:", image_files[:10])
